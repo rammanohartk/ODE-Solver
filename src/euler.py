31a3d62602ebec6c26eb65b9c29dcebd32a55fab
Ram Manohar T K <rammanohartk@gmail.com>
2026-04-27 17:12:15 +0530

    euler method

---
+++ src/euler.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def EulerODE(f,t0,y0,nmax,h):
+    '''
+    # f:function
+    # t0: starting time
+    # y0: starting value
+    # nmax: number of iteration
+    # h : step size
+    # Returns the values of t and y
+    '''
+    t = t0
+    y = y0
+    t_values = [t] # Collect data
+    y_values = [y] # Collect data
+
+    for i in range(1, nmax+1):
+        y = y + f(t,y) * h 
+        t = t + h
+        t_values.append(t)
+        y_values.append(y)
+
+    return t_values,y_values
+
